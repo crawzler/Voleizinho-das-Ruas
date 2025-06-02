f501530420e55420641b6cd93125517f8212a9ee
crawzler <gui-2707@hotmail.com>
2025-06-02 15:20:29 -0300

    VdR-VZ-v0.1.0_Test

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,44 @@
-const CACHE_NAME = 'volei-das-ruas-v0.5.6'; // **IMPORTANTE: Mude este nome a cada nova versão do seu app**
-
+const CACHE_NAME = 'VdR-VZ-v0.1.0';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json',
-  './service-worker.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
-  './images/icon-48x48.png',
-  './images/icon-72x72.png',
-  './images/icon-96x96.png',
-  './images/icon-144x144.png',
-  './images/icon-192x192.png',
-  './images/icon-512x512.png'
+  './css/style.css',
+  './js/script.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         return cache.addAll(urlsToCache);
       })
-      .then(() => self.skipWaiting()) // Esta linha é crucial para a atualização imediata
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
       );
-    })
-    .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        fetch(event.request)
-          .then(networkResponse => {
-            if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
-              const responseToCache = networkResponse.clone();
-              caches.open(CACHE_NAME).then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-            }
-          })
-          .catch(error => {});
-        return response;
-      }
-
-      return fetch(event.request)
-        .then(networkResponse => {
-          if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
-            const responseToCache = networkResponse.clone();
-            caches.open(CACHE_NAME).then(cache => {
-              cache.put(event.request, responseToCache);
-            });
-          }
-          return networkResponse;
-        });
     })
   );
 });
