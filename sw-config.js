5960e7cbfd92883cb82eb83c96782926f5435ecd
guidon <crawzlergg@gmail.com>
2025-08-31 20:26:03 -0300

    VdR-0.6.5_RMK

--- sw-config.js
+++ sw-config.js
@@ -3,7 +3,7 @@
 
 const SW_CONFIG = {
     // Versão do cache - incremente para forçar atualização
-    CACHE_VERSION: 'VdR-0.6.4_RMK',
+    CACHE_VERSION: 'VdR-0.6.4-1_RMK',
     
     // Estratégias de cache
     STRATEGIES: {
